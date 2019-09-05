84d6667e43828fb6a4c7ee3eaf9216f85c222a54
Cardenas Díaz <cpcardenas@indracompany.com>
2019-09-05 17:30:12 -0400

    add  views,assets, others

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
 app.use(express.static(__dirname+'/public'));
- 
-/* app.get('/',(req, res) =>{ */
-  /* res.send('Hello World'); */
-/*   let salida ={
-    nombre: 'claudio',
-    edad: 33,
-    url: req.url
-  }
-  res.send(salida);
-}); */
+//Expresss HBS engine
+hbs.registerPartials( __dirname+'/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/',(req,res)=>{
+  res.render('home',{
+    nombre:'Claudio',
+    anio: new Date().getFullYear()
+
+  });
+})
+app.get('/about',(req,res)=>{
+  res.render('about',{
+    
+    anio: new Date().getFullYear()
+
+  });
+})
 
 app.listen(8080,()=>{
   console.log('Escuchando puerto 8080');
