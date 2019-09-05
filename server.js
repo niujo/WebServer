81b9aa9b441cba35d53a559dc897dfcb5c0b32f7
Cardenas Díaz <cpcardenas@indracompany.com>
2019-09-05 15:11:06 -0400

    agrego css y navbar

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static(__dirname+'/public'));
  
-app.get('/',(req, res) =>{
+/* app.get('/',(req, res) =>{ */
   /* res.send('Hello World'); */
-  let salida ={
+/*   let salida ={
     nombre: 'claudio',
     edad: 33,
     url: req.url
   }
   res.send(salida);
+}); */
+
+app.listen(8080,()=>{
+  console.log('Escuchando puerto 8080');
 });
- 
-app.listen(8080);
